c2a2ae343c0b5b37ba0a81bde2baa19914cba057
DeniseBrise <badro.85@hotmail.fr>
2021-01-14 23:00:08 +0100

    add hero + content

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,33 @@
 <template>
   <div>
+
+    <section class="hero">
+      <div class="hero-body box">
+        <div class="container columns">
+
+          <div class="column">
+            <h1 class="title">
+              Total: {{ totalPrice }} €
+            </h1>
+          </div>
+
+          <div class="column">
+            <b-field label="Nombre de grilles">
+              <b-numberinput v-model="nbGrids" min="0" max="10000" step="1" :exponential=".5" :editable="false" controls-position="compact" controls-rounded></b-numberinput>
+            </b-field>
+          </div>
+
+          <div class="column">
+            <b-field grouped label="Jeu et Options">
+              <b-switch v-model="gridType" passive-type='is-primary'>{{ gridType ? 'Loto' : 'Euro' }}</b-switch>
+              <b-checkbox v-model="isOption">{{ gridType ? '2nd tirage' : 'Etoile+' }}</b-checkbox>
+            </b-field>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
   </div>
 </template>
 
@@ -7,7 +35,21 @@
 export default {
   data() {
     return {
+      nbGrids: 0,
+      gridType: false,
+      isOption: false,
     }
+  },
+  computed: {
+    gridPrice: function() {
+      return this.gridType ? 2.20 : 2.50;
+    },
+    optionPrice: function() {
+      return this.gridType ? 0.80 : 1.00;
+    },
+    totalPrice: function() {
+      return this.nbGrids * (this.gridPrice + (this.isOption ? this.optionPrice : 0));
+    },
   },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Buefy, {
+    defaultIconPack: 'fas',
+    defaultContainerElement: '#content',
+});
 
 new Vue({
   render: h => h(App),
